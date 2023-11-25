3314e65a0301178bc34df5ea88a2ef1ee5b501a5
Yugal D Dekate <yugaldekate72@gmail.com>
2023-11-25 15:36:06 +0530

    setup the project environment

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import './globals.css'
+
+const font = Nunito({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb Clone',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
