28deee8323bcd4268b46cab2f93071465a4fb77e
Yugal D Dekate <yugaldekate72@gmail.com>
2023-12-08 12:39:18 +0530

    created listing page

---
+++ app/listings/[listingId]/ListingClient.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { useMemo } from "react";
+
+import { SafeListing, SafeReservation, SafeUser } from "@/types";
+
+import { categories } from "@/components/navbar/Categories";
+import Container from "@/components/Container";
+import ListingHead from "@/components/listings/ListingHead";
+import ListingInfo from "@/components/listings/ListingInfo";
+
+interface ListingClientProps {
+    reservations?: SafeReservation[];
+    listing: SafeListing & {
+        user: SafeUser;
+    };
+    currentUser?: SafeUser | null;
+}
+
+const ListingClient: React.FC<ListingClientProps> = ( { listing, currentUser } ) => {
+
+    const category = useMemo(() => {
+        return categories.find((items) => (items.label === listing.category) );
+    }, [listing.category]);
+    
+    return ( 
+        <Container>
+            <div className="max-w-screen-lg mx-auto">
+                <div className="flex flex-col gap-6">
+                    <ListingHead
+                        title={listing.title}
+                        imageSrc={listing.imageSrc}
+                        locationValue={listing.locationValue}
+                        id={listing.id}
+                        currentUser={currentUser}
+                    />
+                    <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+                        <ListingInfo
+                            user={listing.user}
+                            category={category}
+                            description={listing.description}
+                            roomCount={listing.roomCount}
+                            guestCount={listing.guestCount}
+                            bathroomCount={listing.bathroomCount}
+                            locationValue={listing.locationValue}
+                        />
+                    </div>
+                </div>
+            </div>
+        </Container>
+    );
+}
+ 
+export default ListingClient;
---
+++ app/listings/[listingId]/page.tsx
@@ -0,0 +1,31 @@
+import ListingClient from "./ListingClient";
+import EmptyState from "@/components/EmptyState";
+
+import getCurrentUser from "@/actions/getCurrentUser";
+import getListingById from "@/actions/getListingById";
+
+
+interface IParams {
+    listingId?: string;
+}
+
+const ListingPage = async ( { params }: { params: IParams } ) => {
+
+    const listing = await getListingById(params);
+    const currentUser = await getCurrentUser();
+
+    if (!listing) {
+        return (
+            <EmptyState />
+        );
+    }
+
+    return ( 
+        <ListingClient
+            listing={listing}
+            currentUser={currentUser}
+        />
+    );
+}
+ 
+export default ListingPage;
